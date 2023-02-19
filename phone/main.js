0d6dba40da34b87f8eb75c0e335bcf6bd4ce3809
Cristina Știru <72434949+cristinastiru@users.noreply.github.com>
2023-02-19 23:24:17 +0200

    Add files via upload

---
+++ phone/main.js
@@ -0,0 +1,8 @@
+
+const button = document.querySelector('.phone-button');
+const screen = document.querySelector('.phone-screen');
+
+button.addEventListener('click', () => {
+  screen.innerHTML = '<p>Dream of SUCCESS and WORK HARD for it!</p>';
+  
+});
